bc5e422183df5f0b46382f301f816ce86aed0b9e
taqpol <njosephchen@gmail.com>
2015-03-09 21:17:32 -0700

    various
    -added ability to save graphs and figures to time-stamped folder
    (hard-coded)
    -added assertion with explanation on globals interaction
    -renamed globals

--- genetic_algorithm_sim.py
+++ genetic_algorithm_sim.py
@@ -4,33 +4,33 @@ from time import time, strftime
 import os
 from pylab import *
 
-global k
-global y
+global payoff
+global pop_size
 global mutation_count
 global recomb_mutate_cycles
-y = 2000
-k = 150
-mutation_count = 600
-recomb_mutate_cycles = 700
+pop_size = 200
+payoff = 150
+mutation_count = 50
+recomb_mutate_cycles = 50
 
 
 def fight(board):
     fitness = []
-    for row in range(y):
-        pool = np.random.randint(0,y-1,y/10)
+    for row in range(pop_size):
+        pool = np.random.randint(0,pop_size-1,pop_size/10)
         one_player_fitness = np.zeros(len(pool))
         for bouts in range(len(pool)):
             player_1_choice = np.random.randint(0,199)
             player_2_choice = np.random.randint(0,199)
             one_player_fitness[bouts] = \
             (board[row,player_1_choice] > board[bouts,player_2_choice])*\
-            (k - board[row,player_1_choice]) + \
+            (pop_size - board[row,player_1_choice]) + \
             (board[row,player_1_choice] < board[bouts,player_2_choice])*\
             (-board[row,player_1_choice]) + \
-            (board[row,player_1_choice] == board[bouts,player_2_choice])*(k/2)
+            (board[row,player_1_choice] == board[bouts,player_2_choice])*(payoff/2)
         fitness.append(one_player_fitness)
     fitness_array = np.asarray(fitness)
-    fitness = np.asarray([numpy.mean(fitness_array[x,]) for x in range(y)])
+    fitness = np.asarray([numpy.mean(fitness_array[x,]) for x in range(pop_size)])
     return fitness
 
     
@@ -38,13 +38,13 @@ def selection(fitness):
     fitness1 = fitness + abs(min(fitness))
     combined = sum(fitness1)
     fitness = fitness1/combined 
-    board_index = np.arange(y)
-    selection = np.random.choice(board_index, y, replace=True, p = fitness)
+    board_index = np.arange(pop_size)
+    selection = np.random.choice(board_index, pop_size, replace=True, p = fitness)
     return selection
 
 
 def mutate(board_copy):
-    selected = np.random.randint(0,y-1, mutation_count)
+    selected = np.random.randint(0,pop_size-1, mutation_count)
     for individuals in selected:
         mutated_value = np.random.randint(0,199)
         board_copy[individuals, mutated_value] = np.random.randint(1,1200)
@@ -53,8 +53,8 @@ def mutate(board_copy):
 
 def crossover(board_copy):
     recombination_point = np.random.randint(0,199)
-    parent1 = np.random.randint(0,y-1)
-    parent2 = np.random.randint(0,y-1)
+    parent1 = np.random.randint(0,pop_size-1)
+    parent2 = np.random.randint(0,pop_size-1)
     chromosome_break_forward1 = board_copy[parent1, recombination_point:]
     chromosome_break_forward2 = board_copy[parent2, recombination_point:]
     chromosome_break_reverse1 = board_copy[parent1, :recombination_point]
@@ -65,27 +65,28 @@ def crossover(board_copy):
         chromosome_break_forward1))
     return board_copy
 
-def figure_creator(current_array, file_number):
-    q = arange(y)
-    chromosome_contents = [numpy.mean(current_array[i,]) for i in range(y)]
+def figure_creator(current_array, file_number, timestamp):
+    q = arange(pop_size)
+    chromosome_contents = [numpy.mean(current_array[i,]) for i in range(pop_size)]
     hist2d(chromosome_contents,q, bins = 60)
-    #os.makedirs(os.path.join("C:\\Users\\Nike\\Desktop\\Graphs\\2D Hist\\", strftime('%c')))
-    savefig('C:\\Users\\Nike\\Desktop\\Graphs\\2D Hist\\%s.png' %file_number)
+    savefig('C:\\Users\\Nike\\Desktop\\Graphs\\2D Hist\\%s\\%s.png' %(timestamp,file_number))
 #    hist(chromosome_contents)
 #    savefig('C:\\Users\\Nike\\Desktop\\Graphs\\1D Hist\\histogram %s.png' %file_number)
 #    scatter(chromosome_contents,q)
 #    savefig('C:\\Users\\Nike\\Desktop\\Graphs\\Scatter\\scatter %s.png' %file_number)
-#    new_directory = os.path.join("C:\\Users\\Nike\\Desktop\\Graphs\\2D Hist\\", strftime('%c'))
-#    os.makedirs(new_directory)
-    np.save('C:\\Users\\Nike\\Desktop\\Graphs\\RawData\\%s' %file_number, current_array)
+    np.save('C:\\Users\\Nike\\Desktop\\Graphs\\RawData\\%s\\%s' %(timestamp,file_number), current_array)
     file_number += 1
     return file_number
     
 def newgen(selection, fight, mutate):
+    assert pop_size >= mutation_count, "mutation_count cannot exceed pop_size"
+    timestamp = strftime('%B %d %Y, %H %M')
+    os.makedirs("C:\\Users\\Nike\\Desktop\\Graphs\\2D Hist\\%s" %timestamp)
+    os.makedirs("C:\\Users\\Nike\\Desktop\\Graphs\\RawData\\%s" %timestamp)
     file_number = 0
-    board = np.random.randint(1,1200,(y,200))
+    board = np.random.randint(1,1200,(pop_size,200))
     current_array = board
-    file_number = figure_creator(current_array, file_number)
+    file_number = figure_creator(current_array, file_number, timestamp)
     for i in range(recomb_mutate_cycles):
         fitness = fight(board)
         indices = selection(fitness)
@@ -93,7 +94,7 @@ def newgen(selection, fight, mutate):
         board_copy = mutate(board_copy)
         board_copy = crossover(board_copy)
         current_array = board_copy
-        file_number = figure_creator(current_array, file_number)
+        file_number = figure_creator(current_array, file_number, timestamp)
     while numpy.array_equal(board,board_copy) == False:
         board = board_copy
         fitness = fight(board)
@@ -101,12 +102,14 @@ def newgen(selection, fight, mutate):
         board_copy = board[indices,]
         board_copy = crossover(board_copy)
         current_array = board_copy
-        file_number = figure_creator(current_array, file_number)
+        file_number = figure_creator(current_array, file_number,timestamp)
     return board_copy
 
 
 then = time()
 f = newgen(selection,fight,mutate)
+hist(f[0,])
 print("Ran in %s seconds" % (time()-then))
 
-#remember to implement a time/date sensitive naming convention
+timestamp = strftime('%B %d %Y, %H %M')
+os.path.join("C:\\Users\\Nike\\Desktop\\Graphs\\2D Hist\\%s" %timestamp)
